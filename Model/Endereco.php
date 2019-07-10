7dd42b293e056c9f502499c62ce92cc2dce95d8a
oliveguga <38103587+oliveguga@users.noreply.github.com>
2019-07-10 07:54:40 -0300

    Update Endereco.php

--- Model/Endereco.php
+++ Model/Endereco.php
@@ -1,5 +1,55 @@
 <?php
 
+if(isset($_POST['cep'])){
+    $cep = filter_input(INPUT_POST, 'cep', FILTER_SANITIZE_SPECIAL_CHARS);
+}
+elseif(isset($_GET['cep'])){
+    $cep = filter_input(INPUT_GET, 'cep', FILTER_SANITIZE_SPECIAL_CHARS);
+}
+
+if(isset($_POST['logradouro'])){
+    $logradouro = filter_input(INPUT_POST, 'logradouro', FILTER_SANITIZE_SPECIAL_CHARS);
+}
+elseif(isset($_GET['logradouro'])){
+    $logradouro = filter_input(INPUT_GET, 'logradouro', FILTER_SANITIZE_SPECIAL_CHARS);
+}
+
+if(isset($_POST['numero'])){
+    $numero = filter_input(INPUT_POST, 'numero', FILTER_SANITIZE_SPECIAL_CHARS);
+}
+elseif(isset($_GET['numero'])){
+    $numero = filter_input(INPUT_GET, 'numero', FILTER_SANITIZE_SPECIAL_CHARS);
+}
+
+if(isset($_POST['complemento'])){
+    $complemento = filter_input(INPUT_POST, 'complemento', FILTER_SANITIZE_SPECIAL_CHARS);
+}
+elseif(isset($_GET['complemento'])){
+    $complemento = filter_input(INPUT_GET, 'complemento', FILTER_SANITIZE_SPECIAL_CHARS);
+}
+
+if(isset($_POST['bairro'])){
+    $bairro = filter_input(INPUT_POST, 'bairro', FILTER_SANITIZE_SPECIAL_CHARS);
+}
+elseif(isset($_GET['bairro'])){
+    $bairro = filter_input(INPUT_GET, 'bairro', FILTER_SANITIZE_SPECIAL_CHARS);
+}
+
+if(isset($_POST['cidade'])){
+    $cidade = filter_input(INPUT_POST, 'cidade', FILTER_SANITIZE_SPECIAL_CHARS);
+}
+elseif(isset($_GET['cidade'])){
+    $cidade = filter_input(INPUT_GET, 'cidade', FILTER_SANITIZE_SPECIAL_CHARS);
+}
+
+if(isset($_POST['uf'])){
+    $uf = filter_input(INPUT_POST, 'uf', FILTER_SANITIZE_SPECIAL_CHARS);
+}
+elseif(isset($_GET['uf'])){
+    $uf = filter_input(INPUT_GET, 'uf', FILTER_SANITIZE_SPECIAL_CHARS);
+}
+
+
 class Endereco{
     private $cep;
     private $logradouro;
